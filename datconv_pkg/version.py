c661dbafc5eafe5e74e90861b0abbf489f3b9436
gwierzchowski <gwierzchowski@wp.pl>
2018-04-20 00:16:31 +0200

    Fixed documentation build

--- datconv_pkg/version.py
+++ datconv_pkg/version.py
@@ -3,7 +3,7 @@
 # Set version number here
 datconv_author  = "Grzegorz Wierzchowski <gwierzchowski@wp.pl>"
 datconv_status  = '5 - Production/Stable'
-datconv_version = '0.7.0'
+datconv_version = '0.7.2'
 datconv_date    = '2018 April'
 
 #Development Status :: 1 - Planning
